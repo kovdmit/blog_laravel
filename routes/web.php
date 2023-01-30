562c0cb58957ff72e73718d5833b56ee54da289e
Dmitry Kovalev <pzhabbiu@gmail.com>
2023-01-30 18:19:36 +0300

    First page: Admin.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['prefix' => 'admin', 'namespace' => '\App\Http\Controllers\Admin'], function ()
+{
+    Route::get('/', 'MainController@index')->name('admin.index');
+}
+);
